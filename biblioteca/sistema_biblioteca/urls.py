37417a29093c1b9ff9839c1413544238c0be90e9
v-t-9 <violeta.a.t.9@gmail.com>
2023-04-13 18:13:20 -0300

    Catálogo

--- biblioteca/sistema_biblioteca/urls.py
+++ biblioteca/sistema_biblioteca/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('catalogo/', views.catalogo, name="catalogo"),
     path('agregar_libro/', views.agregar_libro, name="agregar_libro"),
     path('eliminar_libro/', views.eliminar_libro, name="eliminar_libro"),
     path('agregar_autor/', views.agregar_autor, name="agregar_autor"),
--- biblioteca/sistema_biblioteca/views.py
+++ biblioteca/sistema_biblioteca/views.py
@@ -12,6 +12,50 @@ def index (request):
    
    return render(request,'sistema_biblioteca/index.html', context)
 
+def catalogo(request):
+   print(request.method)
+   catalogo = [
+      {
+         'nombre_autor':'Jorge Luis',
+         'apellido_autor':'Borges',
+         'nombre_libro':'El Aleph',
+         'año_edicion': 2015
+      },
+      {
+         'nombre_autor':'Jorge Luis',
+         'apellido_autor':'Borges',
+         'nombre_libro':'El Inmortal',
+         'año_edicion': 2006
+      },
+
+      {
+         'nombre_autor':'Jorge Luis',
+         'apellido_autor':'Borges',
+         'nombre_libro':'El Sur',
+         'año_edicion': 2020
+      },
+      {
+         'nombre_autor':'Bram',
+         'apellido_autor':'Stoker',
+         'nombre_libro':'Drácula',
+         'año_edicion': 2015
+      },
+      {
+         'nombre_autor':'Alejandro',
+         'apellido_autor':'Dumas',
+         'nombre_libro':'El Conde de Montecristo',
+         'año_edicion': 2010
+      },
+   ]    
+   context = {
+      'user_name': 'Carlos',
+      'user_lastname': 'Lopez',
+      'cat_lista': catalogo,
+   }       
+
+   return render(request,'sistema_biblioteca/catalogo.html', context)
+
+
 def agregar_libro(request):
    return HttpResponse("Agregar libro")
 

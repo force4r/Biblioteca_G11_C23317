fefec50f83ce5735512f05cb2ffe3af0978f319e
v-t-9 <violeta.a.t.9@gmail.com>
2023-05-14 20:09:10 -0300

    cambios en settings.py

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "bibliotecaDB",
         "USER": "postgres",
-        "PASSWORD": "password12345",
+        "PASSWORD": "biblioteca_1234",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }

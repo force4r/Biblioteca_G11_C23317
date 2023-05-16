cf18939d27f92ffe3b8ab982daff3d40120fd8cb
v-t-9 <violeta.a.t.9@gmail.com>
2023-05-16 14:51:37 -0300

    Modificación en models.py

--- biblioteca/sistema_biblioteca/models.py
+++ biblioteca/sistema_biblioteca/models.py
@@ -3,13 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Libro(models.Model):
-    titulo = models.CharField(max_length=100)
-    autor = models.CharField(max_length=200)
-    isbn = models.IntegerField()
-    idioma = models.CharField(max_length=10, default= "Español")
-    año_ingreso = models.IntegerField()
-    año_edicion = models.IntegerField()
+    titulo = models.CharField(max_length=100, verbose_name="Título")
+    autor = models.CharField(max_length=200, verbose_name="Autor")
+    isbn = models.BigIntegerField(verbose_name="ISBN")
+    idioma = models.CharField(max_length=10, default= "Español", verbose_name="Idioma")
+    año_ingreso = models.IntegerField(verbose_name="Año de ingreso")
+    año_edicion = models.IntegerField(verbose_name="Año de edición")
+   
+    
 
 class Autor(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=80)
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    apellido = models.CharField(max_length=80, verbose_name="Apellido")
+    nacionalidad = models.CharField(max_length=15, verbose_name="Nacionalidad", default="nacionalidad")
+
+class Genero(models.Model):
+    genero = models.CharField(max_length=20, verbose_name="Género")
+
+class Editorial(models.Model):
+    editorial = models.CharField(max_length=20, verbose_name="Editorial")

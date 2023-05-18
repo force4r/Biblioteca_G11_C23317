78cf3680efaad1c9f39d170b72c40e53aa2820a1
v-t-9 <violeta.a.t.9@gmail.com>
2023-05-18 17:11:52 -0300

    Relaciones en models.py

--- biblioteca/sistema_biblioteca/models.py
+++ biblioteca/sistema_biblioteca/models.py
@@ -1,24 +1,34 @@
 from django.db import models
 
 # Create your models here.
-
+class Persona(models.Model):
+     nombre = models.CharField(max_length=50, verbose_name="Nombre")
+     apellido = models.CharField(max_length=80, verbose_name="Apellido")
+     class Meta:
+         abstract =True
+class Usuario(Persona):
+    mail=models.EmailField(max_length=128, verbose_name="Email")
+class Autor(Persona):
+    nacionalidad = models.CharField(max_length=15, verbose_name="Nacionalidad", default="nacionalidad")
+    
+class Genero(models.Model):
+    genero = models.CharField(max_length=20, verbose_name="Género", default="Género")
 class Libro(models.Model):
     titulo = models.CharField(max_length=100, verbose_name="Título")
-    autor = models.CharField(max_length=200, verbose_name="Autor")
-    isbn = models.BigIntegerField(verbose_name="ISBN")
-    idioma = models.CharField(max_length=10, default= "Español", verbose_name="Idioma")
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE, default=0) #muchos a uno
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, default=0) #muchos a uno
+    #models.CharField(max_length=200, verbose_name="Autor")
+    isbn = models.BigIntegerField(verbose_name="ISBN") #son 13 digitos
     año_ingreso = models.IntegerField(verbose_name="Año de ingreso")
     año_edicion = models.IntegerField(verbose_name="Año de edición")
+    descripcion = models.CharField(max_length= 3000, verbose_name="Descripcion", default="Descripcion")
+
    
-    
-
-class Autor(models.Model):
-    nombre = models.CharField(max_length=50, verbose_name="Nombre")
-    apellido = models.CharField(max_length=80, verbose_name="Apellido")
-    nacionalidad = models.CharField(max_length=15, verbose_name="Nacionalidad", default="nacionalidad")
-
-class Genero(models.Model):
-    genero = models.CharField(max_length=20, verbose_name="Género")
 
 class Editorial(models.Model):
     editorial = models.CharField(max_length=20, verbose_name="Editorial")
+    Libros=models.ManyToManyField(Libro)
+
+class Idioma(models.Model):
+    idioma = models.CharField(max_length=20, verbose_name="Idioma")
+    Libros=models.ManyToManyField(Libro)

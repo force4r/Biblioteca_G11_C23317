292c1bd480cd795b1f69a445932e30d29010d474
v-t-9 <violeta.a.t.9@gmail.com>
2023-05-21 16:25:23 -0300

    cambios en modelos

--- biblioteca/sistema_biblioteca/models.py
+++ biblioteca/sistema_biblioteca/models.py
@@ -13,22 +13,21 @@ class Autor(Persona):
     
 class Genero(models.Model):
     genero = models.CharField(max_length=20, verbose_name="Género", default="Género")
+class Editorial(models.Model):
+    editorial = models.CharField(max_length=20, verbose_name="Editorial")
 
+class Idioma(models.Model):
+    idioma = models.CharField(max_length=20, verbose_name="Idioma")
 class Libro(models.Model):
     titulo = models.CharField(max_length=100, verbose_name="Título")
     genero = models.ForeignKey(Genero, on_delete=models.CASCADE, default=0) #muchos a uno
-    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, default=0) #muchos a uno
-    #models.CharField(max_length=200, verbose_name="Autor")
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, default=0) #muchos a unopy
     isbn = models.BigIntegerField(verbose_name="ISBN") #son 13 digitos
     año_ingreso = models.IntegerField(verbose_name="Año de ingreso")
     año_edicion = models.IntegerField(verbose_name="Año de edición")
     descripcion = models.CharField(max_length= 3000, verbose_name="Descripcion", default="Descripcion")
+    editoriales = models.ManyToManyField(Editorial)
+    idiomas = models.ManyToManyField(Idioma)
 
-class Editorial(models.Model):
-    editorial = models.CharField(max_length=20, verbose_name="Editorial")
-    libros=models.ManyToManyField(Libro)
 
-class Idioma(models.Model):
-    idioma = models.CharField(max_length=20, verbose_name="Idioma")
-    libros=models.ManyToManyField(Libro)
 

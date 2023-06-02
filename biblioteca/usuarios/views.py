e0a12be250bf40cf9d7607ca17082598d2b0ebac
unknown <lucasmatiasrodriguez93@gmail.com>
2023-06-02 16:49:21 -0300

    se agrego el registro de usuarios

--- biblioteca/usuarios/views.py
+++ biblioteca/usuarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 
 def login_user(request):
@@ -17,6 +19,34 @@ def login_user(request):
             return redirect('login')
         else:
             return render(request, 'authenticate/login.html', context)
+        
+
+def logout_user(request):
+        logout(request)
+        messages.success(request, ("Haz cerrado sesion correctamente"))
+        return redirect('index')    
+
+
+def register_user(request):
+    
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username= form.cleaned_data['username']
+            password= form.cleaned_data['password1']
+            user= authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registro realizado con exito"))
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
+
+        
+        
+        
+    return render (request, 'authenticate/register_user.html',{'form':form})
 
 
 

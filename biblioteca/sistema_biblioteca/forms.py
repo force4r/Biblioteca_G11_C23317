f256c39c48e63ceadd93e04914cc5c3229696a77
v-t-9 <violeta.a.t.9@gmail.com>
2023-06-03 21:05:40 -0300

    cambio año ingreso

--- biblioteca/sistema_biblioteca/forms.py
+++ biblioteca/sistema_biblioteca/forms.py
@@ -130,7 +130,6 @@ class AltaLibro(forms.ModelForm):
     class Meta:
         model = Libro
         fields = '__all__'
-        exclude = ["año_ingreso"]
         widgets = {
             "titulo": forms.TextInput(attrs={'class':'form-control'}),
             "isbn": forms.TextInput(attrs={'class':'form-control'}),

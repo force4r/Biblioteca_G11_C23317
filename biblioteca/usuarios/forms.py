783853569dc34c5c78465ffb9451bc30e8fe9027
Matías Spataro <matiasarielspataro@gmail.com>
2023-06-11 22:06:42 -0300

    cambios en register_user

--- biblioteca/usuarios/forms.py
+++ biblioteca/usuarios/forms.py
@@ -1,1 +1,34 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
+
+class RegisterUserForm(UserCreationForm):
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'class':'form-control'}),
+        required=True
+    )
+    first_name = forms.CharField(
+        min_length= 3, 
+        label="Nombre",
+        widget= forms.TextInput(attrs={'class':'form-control'}),
+        required=True
+    )
+    
+    last_name = forms.CharField(
+        min_length= 3, 
+        label="Apellido",
+        widget= forms.TextInput(attrs={'class':'form-control'}),
+        required=True
+        )
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'

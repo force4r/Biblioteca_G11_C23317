aa6b96722a88e3002f4f0f012cb2188a817f3426
unknown <lucasmatiasrodriguez93@gmail.com>
2023-06-13 00:41:58 -0300

    se agrego dni y legajo al usuario

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -134,5 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
+AUTH_USER_MODEL = 'usuarios.Usuario'
 
 
--- biblioteca/usuarios/admin.py
+++ biblioteca/usuarios/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from usuarios.models import Usuario
+
 
 # Register your models here.
+
+admin.site.register(Usuario)
